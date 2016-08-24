d1518d5ffdce03b646bf2462ee7e8d4b8de177cd
witold.zegarowski <witold.zegarowski@gmail.com>
2016-08-24 11:55:50 +0200

    v2.0

--- _frontend/app/app.js
+++ _frontend/app/app.js
@@ -1,23 +1,20 @@
 angular
-.module('sbApp', [
-	'angularFileUpload', 
-	'ja.qr', 
-	'ngStorage',
-	'ngRoute',
-	'sb.main',
-	'sb.dir'])
-.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-		when('/', {
-			templateUrl: 'src/app/main/main.html',
-			controller: 'mainCtrl'
-		}).
-		when('/dir/:directoryId', {
-			templateUrl: 'src/app/dir/dir.html',
-			controller: 'dirCtrl'
-		}).
-		otherwise({
-			redirectTo: '/'
-		});
-  }]);
+    .module('sbApp', [
+        'angularFileUpload',
+        'ja.qr',
+        'ngStorage',
+        'ngRoute',
+        'sb.main',
+        'sb.dir'])
+    .config(['$routeProvider',
+        function ($routeProvider) {
+            $routeProvider.when('/', {
+                templateUrl: 'main/main.html',
+                controller: 'mainCtrl'
+            }).when('/dir/:directoryId', {
+                templateUrl: 'dir/dir.html',
+                controller: 'dirCtrl'
+            }).otherwise({
+                redirectTo: '/'
+            });
+        }]);
---
+++ _frontend/app/config-prod.js
@@ -0,0 +1,31 @@
+﻿function get_genid($scope, $http, callback) {
+    $http.get($scope.host + '/genid').then(function (data) {
+        console.log("generated uuid", data.data);
+        callback(data.data);
+    });
+}
+
+function get_genuuid($scope, $http, callback) {
+    $http.get($scope.host + '/genuuid').then(function (data) {
+        console.log("generated uuid", data.data);
+        callback(data.data);
+    });
+}
+
+function get_streams($scope, $http, dirId, callback) {
+    $http.get($scope.host + '/status', {
+        params: {
+            dir_uuid: dirId
+        }
+    }).then(function (data) {
+        console.log("get_streams", dirId, '=', data.data);
+        callback(data.data);
+    });
+}
+
+function get_stats($scope, $http, callback) {
+    $http.get($scope.host + '/stats').then(function (data) {
+        console.log("get_stats", data.data);
+        callback(data.data);
+    });
+}
